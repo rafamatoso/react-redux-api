d2b2071bb9261e0b67fac89ffce8acb366165145
Rafael Matoso <rafael.gustavo.gomes@concrete.com.br>
2020-02-10 10:43:44 -0300

    Integracao 2: Duck Pattern, Fetch

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const routes = require('./routes');
+const cors = require('cors');
 
 const app = express();
 const PORT = process.env.PORT || 4000;
@@ -10,14 +11,12 @@ mongoose
     useUnifiedTopology: true,
     useNewUrlParser: true
   })
-  .then(() => console.log('DB server connect'))
-  .catch(e => console.log('DB error', e));
+  .then(() => console.log('DB Server Connected'))
+  .catch(e => console.log('DB Error', e));
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 app.set('PORT', PORT);
 
 module.exports = app;
-
-// app.get('/', (req, res) => res.send('Hello World!'));
-// app.listen(port, () => console.log(`Example app listening on port port!`));
